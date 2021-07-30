249f52cf8d9fe83886f06ae7150b56b94aee7648
Sagar Dalvi <dalvi.sagar.b@gmail.com>
2021-07-30 18:13:46 +0530

    react lifecycle added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SessionApp from "./session/session-app.component";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SessionApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
